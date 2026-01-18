33074cf7e5c819a36f7a6c5cfbaf6f189be9e581
p32929 <p32929@yahoo.com>
2026-01-18 11:24:55 +0600

    Initial Commit

---
+++ boom/boomApp.swift
@@ -0,0 +1,17 @@
+//
+//  boomApp.swift
+//  boom
+//
+//  Created by mac on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct boomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ boomTests/boomTests.swift
@@ -0,0 +1,17 @@
+//
+//  boomTests.swift
+//  boomTests
+//
+//  Created by mac on 18/01/2026.
+//
+
+import Testing
+@testable import boom
+
+struct boomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
